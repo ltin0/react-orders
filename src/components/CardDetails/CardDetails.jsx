c72c3c713bf2c350c42fc7b9dacc6a7e0e9851c9
ltin0 <leo_tino@outlook.com.br>
2022-07-06 21:53:37 -0300

    finished detail pages

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import classes from './CardDetails.module.scss';
+import { toCurrency } from '../../core/number';
 
 import { Container } from '../Container/Container';
 
@@ -11,6 +13,8 @@ export function CardDetails() {
     history.goBack();
   };
   const [orders, GetOrders] = useState({});
+  const [fetching, setFetching] = useState(true);
+
   useEffect(() => {
     fetch(`http://localhost:5000/${params.id}.json`, {
       method: 'GET',
@@ -21,22 +25,77 @@ export function CardDetails() {
       .then(resp => resp.json())
       .then(data => {
         GetOrders(data);
-
-        console.log(data)
+        console.log(data);
+        setFetching(false);
       })
       .catch(err => console.log(err));
   }, []);
 
   return (
     <>
-      <Container>
-        <button onClick={handleBackAct}>Voltar</button>
-        <h1>Olá, {orders.name}</h1>
-        <h2>Número do pedido {orders.id}</h2>
+      {!fetching && (
+        <Container>
+          <>
+            <div className={classes.head}>
+              <h1>Olá, {orders.name}</h1>
+              <button onClick={handleBackAct}>Sair</button>
+            </div>
+            <h2>Número do pedido: {orders.id}</h2>
 
-        <h2>Número do pedido {orders.address.city}</h2>
+            <p>Resumo da compra</p>
+            <hr></hr>
 
-      </Container>
+            {/* <h2>address </h2>
+          <h2>address {orders.items[0].name}</h2> */}
+            {orders.items.map(item => {
+              return (
+                <>
+                  <div className={classes.card}>
+                    <h2>{item.qty}x </h2>
+                    <h2>{item.name}</h2>
+                    <h2>{toCurrency(item.price)}</h2>
+                  </div>
+                  <hr></hr>
+                </>
+              );
+            })}
+            <div className={classes.wraptotal}>
+              <div className={classes.total}>
+                <p>Prazo de entrega </p>
+                <p>Frete </p>
+                <h3>Total :</h3>
+              </div>
+              <div className={classes.total2}>
+                <p>
+                  de {orders.freight?.from} a {orders.freight?.to} dias
+                </p>
+                <p>{toCurrency(orders.freight?.price)}</p>
+                <h3>{toCurrency(orders.total)}</h3>
+              </div>
+            </div>
+            <hr></hr>
+            <div className={classes.freight}>
+              <h2>Acompanhe seu pedido</h2>
+              <ul>
+                <li>{orders.status}</li>
+              </ul>
+              <div className={classes.end}>
+                <h3>Entregar em :</h3>
+                <span>{orders.address?.street} ,</span>
+                <span> {orders.address?.number}</span>
+                <br></br>
+                <span> {orders.address?.city} -</span>
+                <span> {orders.address?.state}</span>
+                <span> {orders.address?.postcode}</span>
+              </div>
+            </div>
+            <div className={classes.payment}>
+              <h3>Forma de pagamento </h3>
+              <p>{orders.payment_method}</p>
+            </div>
+          </>
+        </Container>
+      )}
     </>
   );
 }
